25fa0fb13b23e4f304037595316ba0fe269df90e
Mohammed Khiar <mohammed.khair.allahham@outlook.com>
2024-11-04 02:23:25 +0400

    get task details

---
+++ src/Application/ProjectTasks/Queries/GetTaskDetails/GetTaskDetailsQuery.cs
@@ -0,0 +1,57 @@
+﻿using ProjectManagmentApp.Application.Common.Interfaces;
+using ProjectManagmentApp.Application.Common.Security;
+using ProjectManagmentApp.Domain.Constants;
+
+namespace ProjectManagmentApp.Application.ProjectTasks.Queries.GetTaskDetails;
+
+[Authorize]
+[Authorize(Policy = Policies.CanGet)]
+public record GetTaskDetailsQuery(int Id) : IRequest<TaskDetailsDto>
+{
+}
+
+public class GetTaskDetailsQueryHandler : IRequestHandler<GetTaskDetailsQuery, TaskDetailsDto>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IIdentityService _identityService;
+    private readonly IMapper _mapper;
+    private readonly IUser _user;
+
+    public GetTaskDetailsQueryHandler(IApplicationDbContext context,
+                                         IIdentityService identityService,
+                                         IMapper mapper,
+                                         IUser user)
+    {
+        _context = context;
+        _identityService = identityService;
+        _mapper = mapper;
+        _user = user;
+    }
+    public async Task<TaskDetailsDto> Handle(GetTaskDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var taskEntity = await _context.Tasks.AsNoTracking()
+           .Include(t => t.Project)
+           .Where(l => l.Id == request.Id)
+           .SingleOrDefaultAsync(cancellationToken);
+
+        Guard.Against.NotFound(request.Id, taskEntity);
+
+        // Managers can update tasks, while employees can only update tasks
+        // assigned to them
+        if (_user.Role == Roles.Employee && taskEntity.AssignedTo != _user.Id)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        var projectDto = _mapper.Map<TaskDetailsDto>(taskEntity);
+
+        projectDto.ProjectName = taskEntity.Project.Name;
+        if (projectDto.AssignedTo is not null)
+        {
+            var username = await _identityService.GetUserNameAsync(projectDto.AssignedTo);
+            projectDto.AssignedName = username;
+        }
+
+        return projectDto;
+    }
+}
---
+++ src/Application/ProjectTasks/Queries/GetTaskDetails/TaskDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿using ProjectManagmentApp.Application.Common.Models;
+using ProjectManagmentApp.Domain.Enums;
+
+namespace ProjectManagmentApp.Application.ProjectTasks.Queries.GetTaskDetails;
+public class TaskDetailsDto : BaseProjectAppDto
+{
+    public string? AssignedTo { get; set; }
+    public string? AssignedName { get; set; }
+    public PriorityLevel Priority { get; set; }
+    public ProjectTaskStatus Status { get; set; }
+
+    public int ProjectId { get; set; }
+    public string? ProjectName { get; set; }
+}
